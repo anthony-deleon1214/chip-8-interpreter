cf9767e6eabc40dfba817a838a434dd9c3b2d253
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-10-24 00:28:43 -0400

    Adding final opcode objects

--- instructionSet.ts
+++ instructionSet.ts
@@ -218,6 +218,59 @@ const instructions = [
         arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
     },
     {
-        id: 
-    }
-]
+        id: 'SET_DT_VX',
+        name: 'SET_DT_VX',
+        description: 'Set the delay timer to the value in VX register',
+        mask: 0xF0FF,
+        pattern: 0xF015,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+    {
+        id: 'SET_ST_VX',
+        name: 'SET_ST_VX',
+        description: 'Set the sound timer to the value in the VX register',
+        mask: 0xF0FF,
+        pattern: 0xF018,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+    {
+        id: 'ADD_VX_I',
+        name: 'ADD',
+        description: 'Add the value stored in VX to register I',
+        mask: 0xF0FF,
+        pattern: 0xF01E,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+    {
+        id: 'SET_I_VX',
+        name: 'SET_I_VX',
+        description: 'Set register I to memory address of the sprite data corresponding to the hex digit stored in register VX',
+        mask: 0xF0FF,
+        pattern: 0xF029,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+    {
+        id: 'STO_VX_I',
+        name: 'STO_VX_I',
+        description: 'Store binary-coded decimal equivalent of value in VX at addresses I, I+1, and I+2',
+        mask: 0xF0FF,
+        pattern: 0xF033,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+    {
+        id: 'LD_REG',
+        name: 'LD_REG',
+        description: 'Store values of registers V0 to VX in memory starting from address I, then set I to I+X+1',
+        mask: 0xF0FF,
+        pattern: 0xF055,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+    {
+        id: 'LD_MEM',
+        name: 'LD_MEM',
+        description: 'Fill registers V0 to VX inclusive with values stored in memory from address I, then set I to I+X+1',
+        mask: 0xF0FF,
+        pattern: 0xF065,
+        arguments: [{ mask: 0x0F00, shift: 8, type: 'R' }],
+    },
+];

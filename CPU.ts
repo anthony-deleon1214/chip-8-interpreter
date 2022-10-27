69de06bbf5725614abf023abd7c852950adedafd
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2022-10-27 14:22:47 -0400

    Work on execution cycle for CPU

--- CPU.ts
+++ CPU.ts
@@ -1,3 +1,17 @@
+// Importing disassembler function
+import disassemble from "./disassembler";
+
+interface CPU {
+    memory: Uint8Array,
+    registers: Uint8Array,
+    stack: Uint16Array,
+    SP: number,
+    PC: number,
+    I: number,
+    DT: number,
+    ST: number,
+};
+
 // Creating a CPU class
 class CPU {
     constructor() {
@@ -9,5 +23,47 @@ class CPU {
         this.I = 0;         // Index register
         this.DT = 0;        // Delay timer
         this.ST = 0;        // Sound timer
+    };
+
+    // Reading ROM into buffer
+
+    
+    // Laying out instruction cycle
+    step() {
+        const opcode = this._fetch();
+        const instruction = this._decode(opcode);
+
+        this._execute(instruction);
     }
+
+    _fetch() {
+        return this.memory[this.PC];
+    };
+
+    _decode(opcode) {
+        return disassemble(opcode);
+    };
+
+    _execute(instruction) {
+        // Destructure the object into id and arguments
+        const { id, args } = instruction;
+        switch(id) {
+            case 'CLR':
+                break;
+            case 'RET':
+                break;
+            case 'JP_ADDR':
+                const lastStackIndex = this.stack.f
+                this.stack.set([this.PC])
+                break;
+            case 'CALL_ADDR':
+                break;
+            case 'SE_VX_NN':
+                break;
+            case 'SNE_VX_NN':
+                break;
+            case 'SE_VX_VY':
+                break;
+        }
+    };
 }
